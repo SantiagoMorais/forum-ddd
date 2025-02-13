d67fae5c512764dd8f79c24d990a915edcbe2e66
Felipe Santiago Morais <fillikz@gmail.com>
2025-02-13 15:39:45 -0300

    feat(entities): enhance entity constructors to accept UniqueEntityId and to accepts static method

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,8 +1,19 @@
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityId } from "../../core/entities/unique-entity-id";
 import { IAnswerProps } from "../../core/interfaces/answer-props";
+import { Optional } from "../../core/types/optional";
 
 export class Answer extends Entity<IAnswerProps> {
   get content() {
     return this.props.content;
+  }
+
+  static create(
+    props: Optional<IAnswerProps, "createdAt">,
+    id?: UniqueEntityId
+  ) {
+    const answer = new Answer({ ...props, createdAt: new Date() }, id);
+
+    return answer;
   }
 }
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,4 +1,15 @@
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityId } from "../../core/entities/unique-entity-id";
 import { IQuestionProps } from "../../core/interfaces/question-props";
+import { Optional } from "../../core/types/optional";
 
-export class Question extends Entity<IQuestionProps> {}
+export class Question extends Entity<IQuestionProps> {
+  static create(
+    props: Optional<IQuestionProps, "createdAt">,
+    id?: UniqueEntityId
+  ) {
+    const question = new Question({ ...props, createdAt: new Date() }, id);
+
+    return question;
+  }
+}
