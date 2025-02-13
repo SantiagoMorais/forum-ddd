aea4e5a4d8eead4976bbb4ef050ada1998cf5538
Felipe Santiago Morais <fillikz@gmail.com>
2025-02-13 12:57:08 -0300

    feat(question): add Slug value object and integrate it into Question entity

--- src/core/interfaces/question-props.d.ts
+++ src/core/interfaces/question-props.d.ts
@@ -1,5 +1,8 @@
+import { Slug } from "../../domain/entities/value-objects/slug";
+
 export interface IQuestionProps {
   authorId: string;
   content: string;
+  slug: Slug; // A representation of the question title, without accents and special characters
   title: string;
 }
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,16 +1,19 @@
 import { randomUUID } from "node:crypto";
 import { IQuestionProps } from "../../core/interfaces/question-props";
+import { Slug } from "./value-objects/slug";
 
 export class Question {
   public id: string;
   public title: string;
   public content: string;
+  public slug: Slug;
   public authorId: string;
 
   constructor(props: IQuestionProps, id?: string) {
     this.id = id ?? randomUUID();
     this.title = props.title;
     this.content = props.content;
+    this.slug = props.slug;
     this.authorId = props.authorId;
   }
 }
