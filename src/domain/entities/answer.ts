15d8b95c366ca2cdc7cdb61ef107fd8a5bca3d4e
Felipe Santiago Morais <fillikz@gmail.com>
2025-02-14 07:31:48 -0300

    feat(entities): add new get methods to Answer and Question entities

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -7,6 +7,22 @@ export class Answer extends Entity<IAnswerProps> {
   get content() {
     return this.props.content;
   }
+  
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get questionId() {
+    return this.props.questionId;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
 
   static create(
     props: Optional<IAnswerProps, "createdAt">,
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -2,8 +2,41 @@ import { Entity } from "@/core/entities/entity";
 import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { IQuestionProps } from "@/core/interfaces/question-props";
 import { Optional } from "@/core/types/optional";
+import dayjs from "dayjs";
 
 export class Question extends Entity<IQuestionProps> {
+  get content() {
+    return this.props.content;
+  }
+
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  get title() {
+    return this.props.title;
+  }
+
+  get slug() {
+    return this.props.slug;
+  }
+
+  get isNew(): boolean {
+    return dayjs().diff(this.createdAt, "days") <= 3; // verify if the question was created at most in three days.
+  }
+
   static create(
     props: Optional<IQuestionProps, "createdAt">,
     id?: UniqueEntityId
