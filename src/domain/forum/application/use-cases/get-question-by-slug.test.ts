0de58357180feaf7eb73c0704a32e98585074d3f
Felipe <fillikz@gmail.com>
2025-05-13 17:47:28 -0300

    feat(tests): add makeQuestion factory for creating question instances

--- src/domain/forum/application/use-cases/get-question-by-slug.test.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.test.ts
@@ -1,8 +1,7 @@
+import { makeQuestion } from "@test/factories/make-question";
 import { InMemoryQuestionsRepository } from "@test/repositories/in-memory-questions-repository";
 import { GetQuestionBySlugUseCase } from "./get-question-by-slug";
-import { Question } from "../../enterprise/entities/question";
 import { Slug } from "../../enterprise/entities/value-objects/slug";
-import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
 let sut: GetQuestionBySlugUseCase;
@@ -14,17 +13,11 @@ describe("Get Question By Slug Use Case", () => {
   });
 
   it("Should be able to get a question by its slug", async () => {
-    const newQuestion = Question.create({
-      authorId: new UniqueEntityId(),
-      content: "content",
-      title: "question",
+    const newQuestion = makeQuestion({
       slug: Slug.create("example-question"),
     });
-
     await inMemoryQuestionsRepository.create(newQuestion);
-
     const { question } = await sut.execute({ slug: "example-question" });
-
     expect(question.id).toBeTruthy();
     expect(question.content).toEqual(newQuestion.content);
   });
