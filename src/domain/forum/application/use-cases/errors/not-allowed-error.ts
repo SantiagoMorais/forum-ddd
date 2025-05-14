3f28c828102a8154f662b35d702c8f6b2b171043
Felipe <fillikz@gmail.com>
2025-05-14 16:34:38 -0300

    feat(errors): add IUseCaseError interface and specific error classes for better error handling

---
+++ src/domain/forum/application/use-cases/errors/not-allowed-error.ts
@@ -0,0 +1,8 @@
+import { IUseCaseError } from "@/core/errors/use-case-error";
+
+export class NotAllowedError extends Error implements IUseCaseError {
+  constructor() {
+    super("Not allowed");
+    this.name = "NotAllowedError";
+  }
+}
---
+++ src/domain/forum/application/use-cases/errors/resource-not-found-errror.ts
@@ -0,0 +1,8 @@
+import { IUseCaseError } from "@/core/errors/use-case-error";
+
+export class ResourceNotFoundError extends Error implements IUseCaseError {
+  constructor() {
+    super("Resource not found");
+    this.name = "ResourceNotFoundError";
+  }
+}
