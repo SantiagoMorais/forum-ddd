aa9626c9a68bd7f0653b6f207298205ddd84fdb4
Felipe <fillikz@gmail.com>
2025-05-16 11:24:58 -0300

    feat(Notification): add entity and its interface

---
+++ src/domain/notification/enterprise/entities/notification.ts
@@ -0,0 +1,41 @@
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { INotificationProps } from "@/core/interfaces/notification-props";
+import { Optional } from "@/core/types/optional";
+
+export class Notification extends Entity<INotificationProps> {
+  get recipientId() {
+    return this.props.recipientId;
+  }
+
+  get title() {
+    return this.props.title;
+  }
+
+  get content() {
+    return this.props.content;
+  }
+
+  get readAt() {
+    return this.props.readAt;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  static create(
+    props: Optional<INotificationProps, "createdAt">,
+    id?: UniqueEntityId
+  ) {
+    const notification = new Notification(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id
+    );
+
+    return notification;
+  }
+}
